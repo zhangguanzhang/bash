27b44e5cf4a63aa52f588b32e8a2b66adee33f19
zhangguanzhang <guanzhangzhang@gmail.com>
2018-06-11 15:38:29 +0800

    Create Multi-process.sh

---
+++ Multi-process.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+trap "exec 5>&-;exec 5<$-;exit 0" 2
+
+max_process=2
+
+pipe=`mktemp -u tmp.XXXX`
+mkfifo $pipe
+exec 5<>$pipe
+rm -f $pipe
+
+seq $max_process >&5
+
+start=`date +%s`
+
+for i in {1..10};do
+	read -u5
+	{
+		echo $i;sleep 2;
+		echo >&5
+	}&
+done
+wait
+echo 'Time:' "$((`date +%s`-start))"
+
+exec 5>$-;exec 5<$-
