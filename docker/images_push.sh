746b3b7894544d5dfd3f78282a4f117480d183a7
zhangguanzhang <guanzhangzhang@gmail.com>
2018-07-08 23:31:20 +0800

    Update images_push.sh

--- docker/images_push.sh
+++ docker/images_push.sh
@@ -4,6 +4,7 @@ while :;do
     while read img tag;do
         info=`sudo curl -s "https://hub.docker.com/v2/repositories/$MY_REPO/${img#*/}/tags/$tag/?page_size=1" | jq -r '.name' 2>&1`
         # if the info is null ,the images should be uploaded
+        [ "$tag" == latest ] && { docker push $img:$tag;docker rmi $img:$tag;continue; }
         [[ "$info" =~ null|error ]] && { docker push $img:$tag;docker rmi $img:$tag; } || docker rmi $img:$tag
 
     done < <(docker images --format {{.Repository}}' '{{.Tag}} | grep -P "^$MY_REPO")
