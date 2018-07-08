bc925ca7c91d45362c5017623dbef53bc44c8adf
zhangguanzhang <guanzhangzhang@gmail.com>
2018-07-08 23:28:19 +0800

    Create images_push.sh

---
+++ docker/images_push.sh
@@ -0,0 +1,10 @@
+MY_REPO=zhangguanzhang
+
+while :;do
+    while read img tag;do
+        info=`sudo curl -s "https://hub.docker.com/v2/repositories/$MY_REPO/${img#*/}/tags/$tag/?page_size=1" | jq -r '.name' 2>&1`
+        # if the info is null ,the images should be uploaded
+        [[ "$info" =~ null|error ]] && { docker push $img:$tag;docker rmi $img:$tag; } || docker rmi $img:$tag
+
+    done < <(docker images --format {{.Repository}}' '{{.Tag}} | grep -P "^$MY_REPO")
+done
