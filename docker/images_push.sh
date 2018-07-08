5bea2011d714c9d642361b869fe458c7ee3bb729
zhangguanzhang <guanzhangzhang@gmail.com>
2018-07-08 23:33:26 +0800

    Update images_push.sh

--- docker/images_push.sh
+++ docker/images_push.sh
@@ -1,5 +1,6 @@
 MY_REPO=zhangguanzhang
 
+#如果自己用去掉外层死循环,我是另一个脚本拉取谷歌镜像打成自己仓库的tag后利用这个脚本去推
 while :;do
     while read img tag;do
         info=`sudo curl -s "https://hub.docker.com/v2/repositories/$MY_REPO/${img#*/}/tags/$tag/?page_size=1" | jq -r '.name' 2>&1`
