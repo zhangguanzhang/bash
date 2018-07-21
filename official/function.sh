b4fde9af2942f8e7239a3afe1f4d9a1fc046ff4f
zhangguanzhang <guanzhangzhang@gmail.com>
2018-07-21 19:09:16 +0800

    Update function.sh

--- official/function.sh
+++ official/function.sh
@@ -29,7 +29,39 @@ _quote_readline_by_ref()
     # bash-completion-devel@lists.alioth.debian.org/msg01942.html
     [[ ${!2} == \$* ]] && eval $2=${!2}
 }
+
 [root@k8s-n1 ~]# _quote_readline_by_ref "a'b" test
 [root@k8s-n1 ~]# echo $test
 a\'b
 #----------------------------
+
+#------把除了$1的参数全部赋值给$1------
+# Assign variable one scope above the caller
+# Usage: local "$1" && _upvar $1 "value(s)"
+# Param: $1  Variable name to assign value to
+# Param: $*  Value(s) to assign.  If multiple values, an array is
+#            assigned, otherwise a single value is assigned.
+# NOTE: For assigning multiple variables, use '_upvars'.  Do NOT
+#       use multiple '_upvar' calls, since one '_upvar' call might
+#       reassign a variable to be used by another '_upvar' call.
+# See: http://fvue.nl/wiki/Bash:_Passing_variables_by_reference
+_upvar()
+{
+    if unset -v "$1"; then           # Unset & validate varname
+        if (( $# == 2 )); then
+            eval $1=\"\$2\"          # Return single value
+        else
+            eval $1=\(\"\${@:2}\"\)  # Return array
+        fi
+    fi
+}
+
+[root@k8s-n1 ~]# _upvar test b
+[root@k8s-n1 ~]# echo $test
+b
+[root@k8s-n1 ~]# _upvar test b x y z
+[root@k8s-n1 ~]# echo $test
+b
+[root@k8s-n1 ~]# echo ${test[@]}
+b x y z
+#---------------------
