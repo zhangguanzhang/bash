8a623949056a8b1d599480ca1f70d73da3dc04d7
zhangguanzhang <guanzhangzhang@gmail.com>
2018-07-22 21:10:42 +0800

    some dockerhub v2 api

--- docker/dockerhub.sh
+++ docker/dockerhub.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-#list all the images of my repostry
-curl -s "https://hub.docker.com/v2/repositories/zhangguanzhang/?page_size=100" | jq -r '.results|.[]|.name'
+# Example for the Docker Hub V2 API
+# Requires 'jq': https://stedolan.github.io/jq/
 
-# list the tag of the image
-curl -s "https://hub.docker.com/v2/repositories/zhangguanzhang/kubedash/tags/?page_size=5" | jq -r '.results|.[]|.name'
+UNAME="zhangguanzhang"
+UPASS=""
+repo=zhangguanzhang
+img_name=''
+tag=''
 
+# aquire token
+TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${UNAME}'", "password": "'${UPASS}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+
+# list all the images of my repo
+curl -s https://hub.docker.com/v2/repositories/${repo}/?page_size=100 | jq -r .results[].name
+
+# get all the tags of the image
+curl -s https://hub.docker.com/v2/repositories/${repo}/${img_name}/tags/?page_size=1000 | jq -r '.results[].name'
+
+# delete images and/or tags
+curl -X DELETE -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${repo}/${img_name}/
+curl -X DELETE -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${repo}/${img_name}/tags/${tag}/
