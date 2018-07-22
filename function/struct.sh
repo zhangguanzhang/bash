dec4e7a118b050a0887935d2f55479401f152d8c
zhangguanzhang <guanzhangzhang@gmail.com>
2018-07-22 17:13:43 +0800

    Update struct.sh

--- function/struct.sh
+++ function/struct.sh
@@ -32,8 +32,8 @@ _symbol_intern()
 
 _struct_get()
 {
+    local inst=$1
     local slot_sym=$2
-    local inst=${slot_sym}_$1
     local slot_id=$(_symbol_lookup ${slot_sym}; echo $?)
 
     eval echo \${${inst}[${slot_id}]}
@@ -41,8 +41,8 @@ _struct_get()
 
 _struct_set()
 {
+    local inst=$1
     local slot_sym=$2
-    local inst=${slot_sym}_$1
     local value=$3
     local slot_id=$(_symbol_lookup ${slot_sym}; echo $?)
 
@@ -54,7 +54,7 @@ _defsetter()
     local struct=$1
     local sym_name=$2
 
-    eval "${struct}_${sym_name}_set() { _struct_set \$1 ${sym_name} \$2; }"
+    eval "${struct}_${sym_name}_set() { _struct_set ${struct}_\$1 ${sym_name} \$2; }"
 }
 
 _defgetter()
@@ -62,7 +62,7 @@ _defgetter()
     local struct=$1
     local sym_name=$2
 
-    eval "${struct}_${sym_name}_get() { _struct_get \$1 ${sym_name}; }"
+    eval "${struct}_${sym_name}_get() { _struct_get ${struct}_\$1 ${sym_name}; }"
 }
 
 defstruct()
@@ -78,3 +78,36 @@ defstruct()
         _defgetter ${struct_name} ${sym}
     done
 }
+
+
+shell实现结构体定义和获取值
+参考
+https://gist.github.com/rayfill/3523717
+但是作者39行代码有问题,加了转义斜线后正常
+```bash
+[root@k8s-n1 ~]# source test.shell 
+[root@k8s-n1 ~]# defstruct human age sex
+[root@k8s-n1 ~]# human_age_set guan 22
+[root@k8s-n1 ~]# human_age_set boss 36
+[root@k8s-n1 ~]# human_age_get guan
+22
+[root@k8s-n1 ~]# human_sex_set guan man
+[root@k8s-n1 ~]# human_sex_get guan 
+man
+```
+修改好后突然想起来底层是关联型数组实现的,所以还发现存在下面问题
+```bash
+[root@k8s-n1 ~]# defstruct human age sex
+[root@k8s-n1 ~]# human_age_set guan 22
+[root@k8s-n1 ~]# defstruct cat age sex
+[root@k8s-n1 ~]# cat_age_get guan
+22
+```
+于是修改了57和65行
+```bash
+[root@k8s-n1 ~]# defstruct human age sex
+[root@k8s-n1 ~]# human_age_set guan 22
+[root@k8s-n1 ~]# defstruct cat age sex
+[root@k8s-n1 ~]# cat_age_get guan
+
+```
