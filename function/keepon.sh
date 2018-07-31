3b99b202c0e493dd122a51bae8757eb0e7b6654a
zhangguanzhang <guanzhangzhang@gmail.com>
2018-07-31 21:33:10 +0800

    Update keepon.sh

--- function/keepon.sh
+++ function/keepon.sh
@@ -1,4 +1,4 @@
-#中断后还从上次位置执行,用于长期循环一个列表做动作
+#中断后还从上次位置执行,用于长期循环一个列表做动作,#标记当前应该继续的位置
 $ cat txt
 1 #
 2
@@ -15,3 +15,17 @@ while read repo;do
     sed -i '/'"$repo"'/d' txt;echo "${repo%% *}"' @' >> txt
 done < txt
 sed -ri 's#\s+@##;1s@$@ #@;' txt
+
+
+#----------------------------------------------------
+#上面方法太弱智了...
+$ cat txt
+1
+2
+3
+4
+array=(`xargs -n1 < txt`)
+for i in ${array[@]};do
+  sleep 6
+  sed '/'"$i"'/d' txt;echo "$i" >> txt
+done
