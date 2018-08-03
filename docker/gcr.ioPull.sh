68463aea5045ca66d9f555f2c0afb3ecd296e76d
zhangguanzhang <guanzhangzhang@gmail.com>
2018-08-03 16:25:18 +0800

    Update gcr.ioPull.sh

--- docker/gcr.ioPull.sh
+++ docker/gcr.ioPull.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 set -e
 
-[ -z "$1" ] && Mirror=mirrorgooglecontainers || Mirror=$1
-#gcr.mirrors.ustc.edu.cn
+[ -z "$1" ] && { echo '$1 is not set';exit 2; }
 
-[ -z "$img" ] && { echo '$img is not set';exit 2; }
+if [ "$1" == search ];then
+    shift
+    which jq &> /dev/null || { echo 'search needs jq, please install the jq';exit 2; }
+    img=${1%/}
+    [[ $img == *:* ]] && img_name=${img/://} || img_name=$img
+    if [[ "$img" =~ ^gcr.io|^k8s.gcr.io ]];then
+        if [[ "$img" =~ ^k8s.gcr.io ]];then
+            img_name=${img_name/k8s.gcr.io\//gcr.io/google_containers/}
+        elif [[ "$img" == *google-containers* ]]; then
+            img_name=${img_name/google-containers/google_containers}
+        fi
+    fi
+    #echo '查询用的github,GFW原因可能会比较久,请确保能访问到github'
+    curl -sH 'ref:develop' https://api.github.com/repos/zhangguanzhang/gcr.io/contents/$img_name |
+        jq -r 'length as $len | if $len ==2 then .message elif $len ==12 then .name else .[].name  end'
+else
+    img=$1
+    name=${img%:*}
+    [[ $img == *:* ]] && tag=${img##*:} || tag=latest
 
-[[ "$img" =~ ^gcr.io/google_containers ]] && {
-    docker pull $Mirror"${img:24}"
-    docker tag $Mirror"${img:24}" "$img"
-    docker rmi $Mirror"${img:24}"
-}
+    if [[ "$img" =~ ^gcr.io|^quay.io ]];then
+        repo=zhangguanzhang/${name//\//.}
+        [[ "$img" == *google-containers* ]] && repo=${repo/google-containers/google_containers}
+        docker pull $repo:$tag
+        docker tag $repo:$tag $img
+        docker rmi $repo:$tag
 
-[[ "$img" =~ ^k8s.gcr.io ]] && {
-    docker pull $Mirror"${img:10}"
-    docker tag $Mirror"${img:10}" "$img"
-    docker rmi $Mirror"${img:10}"
-}
+    elif [[ "$img" =~ ^k8s.gcr.io ]];then
+        repo=zhangguanzhang/${name/k8s.gcr.io\//gcr.io.google_containers.}
+        docker pull $repo:$tag
+        docker tag $repo:$tag $img
+        docker rmi $repo:$tag
+    fi
+fi
