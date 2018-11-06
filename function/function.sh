306388c8745ec389d943dbca5934b999dcb6d8a5
zhangguanzhang <guanzhangzhang@gmail.com>
2018-11-06 18:50:13 +0800

    Update function.sh

--- function/function.sh
+++ function/function.sh
@@ -37,3 +37,22 @@ extract() {
          echo "'$1' is not a valid file"
      fi
 }
+
+if test -t 1; then # if terminal
+    ncolors=$(which tput > /dev/null && tput colors) # supports color
+    if test -n "$ncolors" && test $ncolors -ge 8; then
+        termcols=$(tput cols)
+        bold="$(tput bold)"
+        underline="$(tput smul)"
+        standout="$(tput smso)"
+        normal="$(tput sgr0)"
+        black="$(tput setaf 0)"
+        red="$(tput setaf 1)"
+        green="$(tput setaf 2)"
+        yellow="$(tput setaf 3)"
+        blue="$(tput setaf 4)"
+        magenta="$(tput setaf 5)"
+        cyan="$(tput setaf 6)"
+        white="$(tput setaf 7)"
+    fi
+fi
