abc99a5ccc9c42d51443af89ecf0ba15ba20f436
zhangguanzhang <guanzhangzhang@gmail.com>
2019-05-07 14:49:15 +0800

    Update etcd_cron.sh

--- k8s/etcd/etcd_cron.sh
+++ k8s/etcd/etcd_cron.sh
@@ -2,8 +2,9 @@
 set -e
 
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
-
 :  ${bak_dir:=/root/} #缺省备份目录,可以修改成存在的目录
+:  ${cert_dir:=/etc/kubernetes/pki/etcd-a/}
+:  ${endpoints:=https://100.64.2.52:2379,https://100.64.2.53:2379,https://100.64.2.54:2379}
 
 bak_prefix='etcd-'
 cmd_suffix='date +%Y-%m-%d-%H:%M'
@@ -12,7 +13,7 @@ bak_suffix='.db'
 #将规范化后的命令行参数分配至位置参数（$1,$2,...)
 temp=`getopt -n $0 -o c:d: -u -- "$@"`
 
-[ $? != 0 ] && {
+[ $? != 0 ] && { 
     echo '
 Examples:
   # just save once
@@ -29,7 +30,7 @@ set -- $temp
 # -d 指定备份存放目录
 while true;do
     case "$1" in
-        -c)
+        -c) 
             [ -z "$bak_count" ] && bak_count=$2
             printf -v null %d "$bak_count" &>/dev/null || \
                 { echo 'the value of the -c must be number';exit 1; }
@@ -51,19 +52,19 @@ done
 
 function etcd_v2(){
 
-    etcdctl --cert-file /etc/kubernetes/pki/etcd/healthcheck-client.crt --key-file \
-        /etc/kubernetes/pki/etcd/healthcheck-client.key \
-        --ca-file /etc/kubernetes/pki/etcd/ca.crt \
-        --endpoints https://100.64.2.62:2379,https://100.64.2.63:2379,https://100.64.2.64:2379 $@
+    etcdctl --cert-file $cert_dir/healthcheck-client.crt \
+            --key-file $cert_dir/healthcheck-client.key \
+            --ca-file $cert_dir/ca.crt \
+        --endpoints $endpoints $@
 }
 
 function etcd_v3(){
 
     ETCDCTL_API=3 etcdctl   \
-       --cert /etc/kubernetes/pki/etcd/healthcheck-client.crt \
-       --key /etc/kubernetes/pki/etcd/healthcheck-client.key \
-       --cacert /etc/kubernetes/pki/etcd/ca.crt \
-       --endpoints https://100.64.2.62:2379,https://100.64.2.63:2379,https://100.64.2.64:2379 $@
+       --cert $cert_dir/healthcheck-client.crt \
+       --key  $cert_dir/healthcheck-client.key \
+       --cacert $cert_dir/ca.crt \
+       --endpoints $endpoints $@
 }
 
 etcd::cron::save(){
