4425fcad755d419f6b4e5b786805370f16c352e2
zhangguanzhang <zhangguanzhang@qq.com>
2021-01-07 11:57:55 +0800

    Update function.sh

--- function/function.sh
+++ function/function.sh
@@ -56,3 +56,23 @@ if test -t 1; then # if terminal
         white="$(tput setaf 7)"
     fi
 fi
+
+# https://stackoverflow.com/questions/26621647/convert-human-readable-to-bytes-in-bash
+dehumanise() {
+  numfmt --from=iec $@
+#   for v in "${@:-$(</dev/stdin)}"
+#   do  
+#     echo $v | awk \
+#       'BEGIN{IGNORECASE = 1}
+#        function printpower(n,b,p) {printf "%u\n", n*b^p; next}
+#        /[0-9]$/{print $1;next};
+#        /K(iB)?$/{printpower($1,  2, 10)};
+#        /M(iB)?$/{printpower($1,  2, 20)};
+#        /G(iB)?$/{printpower($1,  2, 30)};
+#        /T(iB)?$/{printpower($1,  2, 40)};
+#        /KB$/{    printpower($1, 10,  3)};
+#        /MB$/{    printpower($1, 10,  6)};
+#        /GB$/{    printpower($1, 10,  9)};
+#        /TB$/{    printpower($1, 10, 12)}'
+#   done
+} 
